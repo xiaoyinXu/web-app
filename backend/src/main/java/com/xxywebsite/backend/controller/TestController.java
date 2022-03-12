37764c16edb3911632d63934e0b81987dcce19e7
xuxiaoyin <xuxiaoyin@jd.com>
2022-03-12 20:58:02 +0800

    测试 CI/CD

--- backend/src/main/java/com/xxywebsite/backend/controller/TestController.java
+++ backend/src/main/java/com/xxywebsite/backend/controller/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello From Cookie";
+        return "Hello From Cookie~";
     }
 }
